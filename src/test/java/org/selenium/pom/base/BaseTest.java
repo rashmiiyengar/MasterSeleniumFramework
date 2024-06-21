8f61d7c95d575f44885abf362e7b3ee09670bdcf
rashmisoundar <rashmiiyengar255@gmail.com>
2024-06-21 15:57:13 -0700

    Driver close added

---
+++ src/test/java/org/selenium/pom/base/BaseTest.java
@@ -0,0 +1,19 @@
+package org.selenium.pom.base;
+
+import org.openqa.selenium.WebDriver;
+import org.selenium.pom.factory.DriverManager;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected WebDriver driver;
+    @BeforeMethod
+    public void startDriver(){
+        driver= new DriverManager().initializeDriver();
+    }
+
+    @AfterMethod
+    public void quitDriver(){
+    driver.quit();
+    }
+}
